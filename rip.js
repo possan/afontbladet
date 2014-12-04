33848aff43c5a477350001b1688b810faea1d66b
Per-Olov Jernberg <possan@possan.se>
2014-12-04 23:11:09 +0100

    Cleaner ripper

--- rip.js
+++ rip.js
@@ -8,7 +8,28 @@ page.open('http://www.aftonbladet.se', function() {
 		console.log('jQuery loaded.');
 		var foo = page.evaluate(function() {
 			var l = [];
-			jQuery('*').each(function(i,e) { var t = jQuery(e).text().replace(/[\n\t\r]+/g, ' ').trim().substring(0,100); if (t != '') l.push({ size: parseInt(jQuery(e).css('font-size'), 10), text: t }); });
+			jQuery('img').each(function(i,e) {
+				jQuery(e).remove();
+			});
+			jQuery('script').each(function(i,e) {
+				jQuery(e).remove();
+			});
+			jQuery('noscript').each(function(i,e) {
+				jQuery(e).remove();
+			});
+			jQuery('*:contains("")').each(function(i,e) {
+				var el = jQuery(e);
+				var t = el[0].textContent.replace(/[\n\t\r]+/g, ' ').trim();
+				if (el.children().length == 0) {
+					if (t != '') {
+						l.push({
+							node: el[0].nodeName,
+							size: parseInt(el.css('font-size'), 10),
+							text: t
+						});
+					}
+				}
+			});
 			l.sort(function(a,b) { return b.size - a.size; });
 			console.log(l);
 			return l;

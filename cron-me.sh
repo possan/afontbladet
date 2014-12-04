e770c4c6bdae8d5070801d77407400aaada60115
Per-Olov Jernberg <possan@possan.se>
2014-12-04 21:56:58 +0100

    GZip logs

--- cron-me.sh
+++ cron-me.sh
@@ -15,6 +15,7 @@ if [ -e "text.json" ]; then
 	mkdir -p history
 	BN=history/text-`date +"%Y%m%d-%H%M"`.json
 	cp text.json $BN
+	gzip -9 $BN
 
 	# run bot script and tweet if change
 	/usr/bin/node bot.js >> /tmp/afontbladet.log
